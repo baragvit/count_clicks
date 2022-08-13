13920d84ed5602838782c1369a1f585115b2862c
Vitaliy Baranov <v.baranov@hh.ru>
2022-08-13 18:02:17 +0300

    Shorten url

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import requests
 
-url = 'https://api-ssl.bitly.com/v4/user'
+me_url = 'https://api-ssl.bitly.com/v4/user'
 
 headers = {
     'Authorization': 'Bearer bea9269e049610978b0d58bc7138ad57fb3ab5de'
 }
-response = requests.get(url, headers=headers)
+response = requests.get(me_url, headers=headers)
 print(response.text)
+
+
+
+data = {
+    "long_url": "http://dvmn.org"
+}
+
+shorten_url = 'https://api-ssl.bitly.com/v4/bitlinks'
+response = requests.post(shorten_url, headers=headers, json=data)
+print(response.json())
