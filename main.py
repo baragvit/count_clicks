80eb9882d88f44ce0e2120b463fb560c9cbb4517
Vitaliy Baranov <v.baranov@hh.ru>
2022-08-13 18:51:23 +0300

    Check short url

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import requests
 
 TOKEN = 'bea9269e049610978b0d58bc7138ad57fb3ab5de'
 API_SHORTEN_URL = 'https://api-ssl.bitly.com/v4/bitlinks'
-API_URL_COUNTER = 'https://api-ssl.bitly.com/v4//bitlinks/{}/clicks/summary'
+API_URL_COUNTER = 'https://api-ssl.bitly.com/v4/bitlinks/{}/clicks/summary'
+API_BITLINK_INFO = 'https://api-ssl.bitly.com/v4/bitlinks/{}'
 
 
 def shorten_url(token, url):
@@ -18,7 +19,6 @@ def shorten_url(token, url):
 def count_clicks(token, user_input):
     parsed_url = urlparse(user_input)
     url_without_scheme = parsed_url.netloc + parsed_url.path
-    print(url_without_scheme)
     headers = {'Authorization': f'Bearer {token}'}
     params = {
         "unit": "day",
@@ -33,15 +33,23 @@ def count_clicks(token, user_input):
     return response.json()['total_clicks']
 
 
+def is_bitlink(token, user_url):
+    parsed_url = urlparse(user_url)
+    url_without_scheme = parsed_url.netloc + parsed_url.path
+    headers = {'Authorization': f'Bearer {token}'}
+    response = requests.get(
+        API_BITLINK_INFO.format(url_without_scheme),
+        headers=headers
+    )
+    return response.ok
+
+
 if __name__ == '__main__':
     user_url = input("Please enter url: ")
+    is_link = is_bitlink(TOKEN, user_url)
+    func = count_clicks if is_link else shorten_url
     try:
-        url = shorten_url(TOKEN, user_url)
-        print('Битлинк', url)
-    except requests.exceptions.HTTPError:
+        result = func(TOKEN, user_url)
+        print(result if is_link else 'Битлинк: ' + result)
+    except requests.exceptions.HTTPError as err:
         print("You have entered incorrect url")
-
-    try:
-        print(count_clicks(TOKEN, url))
-    except requests.exceptions.HTTPError:
-        print("You have entered incorrect URL")
